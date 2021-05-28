0a775a3709739cd4f247b8dedff041fc6ab04950
XyvenQ <maritimus987@gmail.com>
2021-05-28 16:18:27 -0700

    Now breaks down analytics based on category

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import fs from "fs"
 import google from "googleapis"
 import moment from "moment"
+import momentDurationFormat from "moment-duration-format"
+
 const config = JSON.parse(fs.readFileSync('config.json', 'utf8'))
 
 var SCOPES = ['https://www.googleapis.com/auth/youtube.readonly'];
@@ -8,13 +10,25 @@ var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
     process.env.USERPROFILE) + '/.credentials/';
 var TOKEN_PATH = TOKEN_DIR + 'youtube-nodejs-quickstart.json';
 
-function groupBy(list) {
+function countOccurnces(list) {
   const map = new Map()
   list.forEach(item => {
     map.set(item, (map.get(item)||0) + 1)
   })
   return map
-}
+} 
+Object.defineProperty(Object.prototype, "map", {
+  value: function map(fn) {
+    return Object.fromEntries(
+      Object.entries(this).map(
+        ([k, v], i) => [k, fn(v, k, i)]
+      )
+    )
+  },
+  writable: true,
+  configurable: true
+})
+
 function authorize(credentials, callback) {
   var clientSecret = credentials.installed.client_secret
   var clientId = credentials.installed.client_id
@@ -39,7 +53,7 @@ function getData(auth) {
 
   var videoData = fs.readFileSync(config.dataPath, 'utf8')
   var videos = videoData.match(/(?<=href="https:\/\/www.youtube.com\/watch\?v=)([^"]+)/g)
-  var videosWatchCount = [...groupBy(videos).entries()].sort((a, b) => b[1] - a[1])
+  var videosWatchCount = [...countOccurnces(videos).entries()].sort((a, b) => b[1] - a[1])
 
   var t0 = process.hrtime()
 
@@ -73,9 +87,26 @@ function getData(auth) {
 
 function analyzeData(){
   var videoData = fs.readFileSync(config.dataPath, 'utf8')
-  var localVideoData = JSON.parse(fs.readFileSync('videoData.json', 'utf8'))
+  var categoryIdMap = JSON.parse(fs.readFileSync('videoCategoryIdList.json'))
+  var apiVideoData = JSON.parse(fs.readFileSync('videoData.json', 'utf8'))
   var videos = videoData.match(/(?<=href="https:\/\/www.youtube.com\/watch\?v=)([^"]+)/g)
-  var videosWatchCount = [...groupBy(videos).entries()].sort((a, b) => b[1] - a[1])
-
-  console.log()
+  var videosWatchCount = countOccurnces(videos)
+  
+  var restructuredData = apiVideoData.map(v=>({
+    id: v.id,
+    durationS: moment.duration(v.contentDetails.duration).asSeconds(),
+    type: v.liveStreamingDetails ? 'livestream' : 'video',
+    category: categoryIdMap[v.snippet.categoryId||0],
+    channelId: v.snippet.channelId,
+    channelTitle: v.snippet.channelTitle,
+    viewCount: v.statistics.viewCount,
+    publishedAt: Date.parse(v.snippet.publishedAt),
+    watchCount: videosWatchCount.get(v.id)
+  }))
+  var totalTime = moment.duration(restructuredData.reduce((a, v)=> a + (v.type=='video')*v.durationS, 0),  'seconds').format()
+  var totalTimeByCategory = restructuredData.reduce((a,v)=>{
+    if(v.type=='video') a[v.category] = a[v.category] ? a[v.category] + v.durationS : v.durationS
+    return a
+  }, {}).map(v=>moment.duration(v, 's').format())
+  console.log("Total watch time: "  + totalTime + '\n' + JSON.stringify(totalTimeByCategory, null, 2))
 }
